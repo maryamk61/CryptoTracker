1aad0e30162188b136980446f099e10343ab5cb4
Maryam Kaveh <maryamk61@gmail.com>
2023-07-21 13:29:55 +0330

    Adding Launch screen with animation

---
+++ CryptoTracker/Base/Launch/VIews/LaunchView.swift
@@ -0,0 +1,66 @@
+//
+//  LaunchView.swift
+//  CryptoTracker
+//
+//  Created by Maryam Kaveh on 4/28/1402 AP.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    // convert to array to animate letters individually
+    @State private var loadingText: [String] = "Loading your portfolio...".map { String($0) }
+    @State private var showText: Bool = false
+    let timer = Timer.publish(every: 0.08, on: .main, in: .common).autoconnect()
+    @State private var counter: Int = 0
+    @State private var loops = 0
+    @Binding var showLaunchView: Bool// make it @Binding to bind it to the rest of the app
+    
+    var body: some View {
+        ZStack {
+            Color.launch.background
+                .ignoresSafeArea()
+            Image("logo-transparent")
+                .resizable()
+                .frame(width: 100, height: 100)
+            ZStack {
+                if showText {
+                    HStack(spacing: 0) {
+                        ForEach(loadingText.indices) { index in
+                            Text(loadingText[index])
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color.launch.accent)
+                                .offset(y: counter == index ? -7 : 0)
+                        }
+                    }
+                    .transition(.scale.animation(.easeOut))
+                }
+                
+            }
+            .offset(y: 70)
+        }
+        .onAppear {
+            showText.toggle()
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.spring()) {
+                if counter == (loadingText.count - 1) {
+                    counter = 0
+                    loops += 1
+                    if loops == 2 {
+                      showLaunchView = false
+                    }
+                } else {
+                    counter += 1
+                }
+            }
+        }
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView(showLaunchView: .constant(true))
+    }
+}
--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -10,7 +10,8 @@ import SwiftUI
 @main
 struct CryptoTrackerApp: App {
   @StateObject private var viewModel = HomeViewModel()
-  
+    @State private var showLaunchView: Bool = true
+    
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
@@ -18,12 +19,22 @@ struct CryptoTrackerApp: App {
     
     var body: some Scene {
         WindowGroup {
-          NavigationView {
-            HomeView()
-//              .navigationBarHidden(true)
-              .navigationBarBackButtonHidden(false)
-          }
-          .environmentObject(viewModel)
+            ZStack {
+                NavigationView {
+                    HomeView()
+                    //.navigationBarHidden(true)
+                        .navigationBarBackButtonHidden(false)
+                }
+                .environmentObject(viewModel)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0) // always on top of the NavigationView
+            }
         }
       
     }
